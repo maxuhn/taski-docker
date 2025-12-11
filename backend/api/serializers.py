2e87e11d64ca0d3321df9047b343db866aa2fc35
Max Uhn <kolosoff0730@gmail.com>
2025-12-11 12:48:19 +0700

    Update workflow

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'completed')
+        fields = ("id", "title", "description", "completed")
